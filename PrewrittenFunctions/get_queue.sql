7b9f58145f08c1882f824a608c3facd191d59586
zhansu98 <79766316+zhansu98@users.noreply.github.com>
2021-05-12 22:36:01 -0400

    Update get_queue.sql

--- PrewrittenFunctions/get_queue.sql
+++ PrewrittenFunctions/get_queue.sql
@@ -1,5 +1,5 @@
 -- gets queue in a specific room by play order
-SELECT DISTINCT q.play_order, song_name
+SELECT DISTINCT q.play_order
 FROM in_queue q, song s
 -- sub room id with variable
 WHERE queue_rooms_room_id = 1
